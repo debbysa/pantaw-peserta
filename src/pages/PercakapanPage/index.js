55abf395517de71a2b6bb2286f97fdde1a6458db
debbysa <deby.silvia23@gmail.com>
2019-05-23 11:58:45 +0700

    mengatur tampilan percakapan

--- src/pages/PercakapanPage/index.js
+++ src/pages/PercakapanPage/index.js
@@ -3,6 +3,9 @@ import socketIOClient from "socket.io-client";
 import axios from "axios";
 import styled from "styled-components";
 
+import Button from "../../components/Button";
+import TextInput from "../../components/TextInput";
+
 export default class PercakapanPage extends Component {
   state = {
     percakapan: [],
@@ -10,10 +13,12 @@ export default class PercakapanPage extends Component {
   };
 
   socket = socketIOClient("http://localhost:3000");
+  detail_workshop = JSON.parse(localStorage.getItem("detail_workshop"));
 
   componentDidMount() {
+    const { id_workshop } = this.detail_workshop;
     axios
-      .get("http://localhost:3000/workshop/1/percakapan")
+      .get(`http://localhost:3000/workshop/${id_workshop}/percakapan`)
       .then(response => this.setState({ percakapan: response.data }));
 
     this.socket.on("percakapan", percakapan => {
@@ -26,37 +31,60 @@ export default class PercakapanPage extends Component {
   }
 
   sendMessage() {
+    const { id_workshop, id_peserta } = this.detail_workshop;
     axios
-      .post("http://localhost:3000/workshop/1/percakapan", {
-        id_pengirim:
-          "eyJhbGciOiJIUzI1NiJ9.ZGVieWVtb24.6k_EkhxAuI_shcHTmXnUjw93zrctsqtILDZgNOm08gc",
+      .post(`http://localhost:3000/workshop/${id_workshop}/percakapan`, {
+        id_pengirim: id_peserta,
         pesan: this.state.message
       })
       .then(response => {
-        this.socket.emit("percakapan", 1);
+        this.socket.emit("percakapan", id_workshop);
         this.setState({ message: "" });
       });
   }
 
   render() {
+    const token = localStorage.getItem("token");
+    if (!token) window.location.replace("/login");
+
+    const detail_workshop = JSON.parse(localStorage.getItem("detail_workshop"));
+    const { peserta } = detail_workshop;
+
     return (
       <Body>
-        <input
-          type="text"
-          value={this.state.message}
-          onChange={event => this.changeMessage(event.target.value)}
-        />
-        <button onClick={() => this.sendMessage()}>Kirim</button>
-        {this.state.percakapan.map(item => (
-          <Item>
-            <li>
+        <InputContainer>
+          <TextInput
+            label="Pesan"
+            value={this.state.message}
+            onChange={value => this.changeMessage(value)}
+          />
+          <Button onClick={() => this.sendMessage()} text="Kirim" />
+        </InputContainer>
+
+        <MessageContainer>
+          {this.state.percakapan.map(item => (
+            <Item
+              style={{
+                alignSelf: item.peserta
+                  ? item.peserta.nama === peserta.nama
+                    ? "flex-end"
+                    : "flex-start"
+                  : "flex-start",
+
+                backgroundColor: item.peserta
+                  ? item.peserta.nama === peserta.nama
+                    ? "#2285e3"
+                    : "#08c21c"
+                  : "#08c21c"
+              }}
+            >
               {item.peserta
                 ? item.peserta.nama
                 : item.pemateri.nama + " (pemateri) "}
               <p>{item.pesan}</p>
-            </li>
-          </Item>
-        ))}
+            </Item>
+          ))}
+        </MessageContainer>
       </Body>
     );
   }
@@ -67,6 +95,25 @@ const Body = styled.div`
   display: flex;
 `;
 
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const MessageContainer = styled.div`
+  display: flex;
+  margin: auto;
+  flex-direction: column;
+  align-items: center;
+  height: 200px;
+  width: 500px;
+  overflow-x: scroll;
+`;
+
 const Item = styled.ul`
-  /* padding: 2em 2em; */
+  background-color: #2285e3;
+  color: white;
+  padding: 20px;
+  border-radius: 5px;
 `;
